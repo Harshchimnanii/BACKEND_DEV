79e19bc721b0936be625fb41fddb4bbcf7b81b7b
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-01-22 10:50:18 +0530

    Saving all work to main branch

--- Classroom-practice/DAY_1/server.js
+++ Classroom-practice/DAY_1/server.js
@@ -1,10 +1,11 @@
-const http= require("http");
-const server =http.createServer((req,res)=>{
-    res.writeHead(200,{"Content-Type":"text/html"});
-    res.end("Server is running")
-})
+const http = require("http");
 
-Server.listen(8000,()=>{
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end("Server is running");
+});
+
+// Changed 'Server' to 'server' to match the variable name above
+server.listen(8000, () => {
     console.log("Server is running on port 8000");
-
-})
+});
