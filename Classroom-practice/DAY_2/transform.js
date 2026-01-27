34831a8348768c959dbede78ec355dfaead5b1e4
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-01-27 11:49:42 +0530

    Classroom Practice Update - 27 Jan 2026

--- Classroom-practice/DAY_2/transform.js
+++ Classroom-practice/DAY_2/transform.js
@@ -1,11 +1,21 @@
-const transformOutputfilePath = path.join(__dirname, "transformoutput.txt");
-const readStream = fs.CreateReadStream(inputFilePath);
-const { Transform } = require('stream');
-const writeStream = fs.createWriteStream(transformOutputFilePath);
-const upperCaseTransform = new Transform({
-    transform(chunk, encoding, callback) {
-        const transformedData = chunk.toString().toUpperCase();
-        callback(null, transformedData);
-    }
-});
+const fs=require("fs");
+const path=require("path");
+const { Transform } = require("stream");
+
+
+const inputFilePath=path.join(__dirname,"input.txt");
+const transformOutputFilePath=path.join(__dirname,"transformOutputFilePath");
+
+const readStream=fs.createReadStream(inputFilePath);
+const writeStream=fs.createWriteStream(transformOutputFilePath);
+
+
+const upperCaseTransform=new Transform({
+   transform(chunk,encoding,callback){
+    const transformedChunk=chunk.toString().toUpperCase();
+    this.push(transformedChunk);
+    callback();
+   }
+})
+
 readStream.pipe(upperCaseTransform).pipe(writeStream);
