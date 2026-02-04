a1f1ec85b89552ec16f9330ade12321b72065b98
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-02-04 10:51:45 +0530

    day 3 feb updated

--- Expressjs-backend/index.js
+++ Expressjs-backend/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-// Corrected syntax: Use colons (:) inside objects, not semicolons (;)
 const students = [
     { id: 1, name: "raj", branch: "CSE" },
     { id: 2, name: "yash", branch: "ECE" },
@@ -17,10 +16,8 @@ app.get("/students", (req, res) => {
     res.json(students);
 });
 
-// Fixed 'red' to 'req' and 'requestAnimationFrame' to 'req.params'
 app.get("/students/:id", (req, res) => {
     const id = req.params.id;
-    // Find the student whose ID matches the URL parameter
     const student = students.find(s => s.id == id);
 
     if (student) {
